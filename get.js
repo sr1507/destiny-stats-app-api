8a74fdd320d1491acf29ba5502e71068ceaebc95
sr1507 <sam10rodgers@gmail.com>
2020-05-30 18:48:33 +0100

    Added functionality to allow only records after specified date to be returned

--- get.js
+++ get.js
@@ -1,41 +1,48 @@
 import "./libs/algorithms-lib.js";
-import {calculateKD, calculateKAD, getTotals} from "./libs/algorithms-lib.js";
+import { calculateKD, calculateKAD, getTotals } from "./libs/algorithms-lib.js";
+import moment from "moment";
+import { findIndexOfEarliestActivityWithinStartDate } from "./libs/utilities-lib.js";
 
 export async function main(event, context, callback) {
 
-  const axios = require('axios');
+  const axios = require("axios");
 
   let config = {
-    'X-API-KEY': process.env.bungieApiKey
+    "X-API-KEY": process.env.bungieApiKey,
   };
 
   const rootPath = "https://www.bungie.net/Platform/Destiny2/";
-  console.log(event.queryStringParameters);
   const membershipType = event.queryStringParameters.membershipType;
   const membershipId = event.queryStringParameters.membershipId;
+  const startDate = moment(event.queryStringParameters.startDate);
 
-  await axios.get(rootPath + membershipType + "/Profile/" + membershipId + "/?components=100", {headers: config}).then(profileResponse => getProfileActivities(profileResponse)).then(profileActivities => {
-    var statsResponse = {};
-    statsResponse.totals = getTotals(profileActivities);
-    statsResponse.killsDeathsRatio = calculateKD(statsResponse.totals);
-    statsResponse.killsAssistsDeathsRatio = calculateKAD(statsResponse.totals);
+  await axios
+    .get(rootPath + membershipType + "/Profile/" + membershipId + "/?components=100", { headers: config })
+    .then((profileResponse) => getProfileActivities(profileResponse))
+    .then((profileActivities) => {
+      var statsResponse = {};
+      statsResponse.totals = getTotals(profileActivities);
+      statsResponse.killsDeathsRatio = calculateKD(statsResponse.totals);
+      statsResponse.killsAssistsDeathsRatio = calculateKAD(statsResponse.totals);
 
-    var apiResponse = {
-      "isBase64Encoded": false,
-      "statusCode": 200,
-      "headers": { "Access-Control-Allow-Origin": "*", "Access-Control-Allow-Credentials": true },
-      "body": JSON.stringify(statsResponse)
-    };
-    callback(null, apiResponse);
-  }).catch(e => {
+      var apiResponse = {
+        isBase64Encoded: false,
+        statusCode: 200,
+        headers: { "Access-Control-Allow-Origin": "*", "Access-Control-Allow-Credentials": true },
+        body: JSON.stringify(statsResponse),
+      };
 
-    console.log(e);
-    callback(e);
-  });
+
+      callback(null, apiResponse);
+    })
+    .catch((e) => {
+      console.log(e);
+      callback(e);
+    });
 
   function getProfileActivities(profileResponse) {
     return new Promise((resolve, reject) => {
-      Promise.all(getCharacterActivityPromises(profileResponse.data.Response.profile.data.characterIds)).then(responseArray => {
+      Promise.all(getCharacterActivityPromises(profileResponse.data.Response.profile.data.characterIds)).then((responseArray) => {
         var profileActivities = [];
         for (var i = 0; i < responseArray.length; i++) {
           profileActivities = profileActivities.concat(responseArray[i]);
@@ -51,7 +58,7 @@ export async function main(event, context, callback) {
       const characterActivityPromise = new Promise((resolve, reject) => {
         var character = {};
         character.characterId = characterIds[i];
-        getCharacterActivities(characterIds[i]).then((characterActivities) => {
+        getCharacterActivities(characterIds[i], startDate).then((characterActivities) => {
           character.activities = characterActivities;
           resolve(character);
         });
@@ -61,14 +68,27 @@ export async function main(event, context, callback) {
     return characterActivityPromises;
   }
 
-  function getCharacterActivities(characterId, page = 0, activities = []) {
+  function getCharacterActivities(characterId, startDate, page = 0, activities = []) {
     return new Promise((resolve, reject) => {
       const url = rootPath + membershipType + "/Account/" + membershipId + "/Character/" + characterId + "/Stats/Activities/?count=250&mode=5&page=" + page;
-      axios.get(url, {headers: config}).then(activitiesResponse => {
+      axios.get(url, {headers: config}).then((activitiesResponse) => {
+        var activityDate = {};
         if (activitiesResponse.data.Response.activities) {
-          activities = activities.concat(activitiesResponse.data.Response.activities);
-          resolve(getCharacterActivities(characterId, ++page, activities));
+          activityDate = moment(activitiesResponse.data.Response.activities[activitiesResponse.data.Response.activities.length - 1].period);
+          if (activityDate.isBefore(startDate)) {
+            // Only some of the activities on this page can be used
+            var finalIndex = findIndexOfEarliestActivityWithinStartDate(activitiesResponse.data.Response.activities, startDate);
+            if (finalIndex >= 0) {
+              activities = activities.concat(activitiesResponse.data.Response.activities.slice(0, finalIndex + 1));
+            }
+            resolve(activities);
+          } else {
+            // All activities on this page can be used
+            activities = activities.concat(activitiesResponse.data.Response.activities);
+            resolve(getCharacterActivities(characterId, startDate, ++page, activities));
+          }
         } else {
+          // No activities returned on page
           resolve(activities);
         }
       });
