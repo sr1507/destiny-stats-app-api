69ba63d9ed8d864ca2bcb1b8e18d076b1f3abece
sr1507 <sam10rodgers@gmail.com>
2020-06-02 21:22:27 +0100

    Added build stats function

--- libs/utilities-lib.js
+++ libs/utilities-lib.js
@@ -1,29 +1,41 @@
 import moment from "moment";
+import { calculateKDs, calculateKDAs, getTotals, calculateEfficiency, calculateKillsPGAs, calculateWinRates } from "./algorithms-lib.js";
 
 export function findIndexOfEarliestActivityWithinStartDate(activities, startDate) {
-    var jump = activities.length;
-    var currentIndex = 0;
+  var jump = activities.length;
+  var currentIndex = 0;
 
+  jump = Math.round(jump / 2);
+  currentIndex += jump;
+
+  while (jump > 1) {
     jump = Math.round(jump / 2);
-    currentIndex += jump;
 
-    while (jump > 1) {
-
-      jump = Math.round(jump / 2);
-
-      if ((moment(activities[currentIndex].period)).isAfter(startDate)) {
-        currentIndex += jump;
-        if (currentIndex >= activities.length) {
-          currentIndex = activities.length - 1;
-        }
-      } else if ((moment(activities[currentIndex].period)).isBefore(startDate)) {
-        currentIndex -= jump;
-      } else {
-        while(!(currentIndex == activities.length - 1 || (moment(activities[currentIndex + 1].period)).isBefore(startDate))){
-          currentIndex += 1;
-        }
-        return currentIndex;
+    if (moment(activities[currentIndex].period).isAfter(startDate)) {
+      currentIndex += jump;
+      if (currentIndex >= activities.length) {
+        currentIndex = activities.length - 1;
       }
+    } else if (moment(activities[currentIndex].period).isBefore(startDate)) {
+      currentIndex -= jump;
+    } else {
+      while (!(currentIndex == activities.length - 1 || moment(activities[currentIndex + 1].period).isBefore(startDate))) {
+        currentIndex += 1;
+      }
+      return currentIndex;
     }
-    return currentIndex;
   }
+  return currentIndex;
+}
+
+export function buildStats(activities) {
+  var stats = {};
+  stats.totals = getTotals(activities);
+  stats.killsDeathsRatio = calculateKDs(stats.totals);
+  stats.efficiency = calculateEfficiency(stats.totals);
+  stats.killsDeathsAssists = calculateKDAs(stats.totals);
+  stats.averageKills = calculateKillsPGAs(stats.totals);
+  stats.winRate = calculateWinRates(stats.totals);
+
+  return stats;
+}
--- tests/utilities-lib.test.js
+++ tests/utilities-lib.test.js
@@ -1,4 +1,4 @@
-import {findIndexOfEarliestActivityWithinStartDate} from "../libs/utilities-lib";
+import {findIndexOfEarliestActivityWithinStartDate,buildStats} from "../libs/utilities-lib";
 
 var activities = [
   {
@@ -67,3 +67,81 @@ test("test 7", () => {
   var index = findIndexOfEarliestActivityWithinStartDate(activities, startDate);
   expect(index).toEqual(0);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+let response = require("../mocks/algorithms-test.json");
+var expectedStats = {totals:{
+  gamemodeList: [
+     5, 70, 10, 73,
+    84, 69, 37
+  ],
+  kills: {
+    '5': 105,
+    '10': 25,
+    '37': 59,
+    '69': 59,
+    '70': 25,
+    '73': 25,
+    '84': 21
+  },
+  deaths: {
+    '5': 93,
+    '10': 16,
+    '37': 52,
+    '69': 52,
+    '70': 16,
+    '73': 16,
+    '84': 25
+  },
+  assists: { 
+    '5': 31,
+    '10': 3, 
+    '37': 22, 
+    '69': 22, 
+    '70': 3, 
+    '73': 3, 
+    '84': 6 
+  },
+  score: {
+    '5': 70,
+    '10': 36,
+    '37': 17,
+    '69': 17,
+    '70': 36,
+    '73': 36,
+    '84': 17
+  },
+  timePlayedSeconds: {
+    '5': 6368,
+    '10': 648,
+    '37': 3631,
+    '69': 3631,
+    '70': 648,
+    '73': 648,
+    '84': 2089
+  },
+  wins: { '5': 6, '10': 0, '37': 3, '69': 3, '70': 0, '73': 0, '84': 3 },
+  losses: { '5': 4, '10': 1, '37': 2, '69': 2, '70': 1, '73': 1, '84': 1 },
+  gamesPlayed: { '5': 10, '10': 1, '37': 5, '69': 5, '70': 1, '73': 1, '84': 4 }
+},
+    killsDeathsRatio : { '5': 1.12903, '10': 1.5625, '37': 1.13462, '69': 1.13462, '70': 1.5625, '73': 1.5625, '84': 0.84 },
+    efficiency : { '5': 1.46237, '10': 1.75, '37': 1.55769, '69': 1.55769, '70': 1.75, '73': 1.75, '84': 1.08 },
+    killsDeathsAssists : { '5': 1.2957, '10': 1.65625, '37': 1.34615, '69': 1.34615, '70': 1.65625, '73': 1.65625, '84': 0.96 },
+    averageKills : { '5': 10.5, '10': 25, '37': 11.8, '69': 11.8, '70': 25, '73': 25, '84': 5.25 },
+    winRate : { '5': 0.6, '10': 0, '37': 0.6, '69': 0.6, '70': 0, '73': 0, '84': 0.75 }
+}
+
+test("Build Stats", () => {
+    var stats = buildStats(response.Response.activities);
+    expect(JSON.stringify(stats)).toEqual(JSON.stringify(expectedStats));
+  });
