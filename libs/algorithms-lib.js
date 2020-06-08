ffe4e4fb96ba9ff69fc51dc740a9de632e85f47d
sr1507 <sam10rodgers@gmail.com>
2020-06-08 18:25:51 +0100

    Fixed bug with rumble winrate

--- libs/algorithms-lib.js
+++ libs/algorithms-lib.js
@@ -32,7 +32,7 @@ export function calculateKDAs(totals) {
     if (totals.deaths[mode] == 0) {
       KADs.mode = totals.kills[mode] + totals.assists[mode];
     } else {
-      KADs[mode] = round((totals.kills[mode] + (totals.assists[mode]) / 2) / totals.deaths[mode]);
+      KADs[mode] = round((totals.kills[mode] + totals.assists[mode] / 2) / totals.deaths[mode]);
     }
   }
 
@@ -70,9 +70,18 @@ export function getTotals(activities) {
         totals.score[mode] += activity.values.score.basic.value;
         totals.timePlayedSeconds[mode] += activity.values.timePlayedSeconds.basic.value;
         totals.gamesPlayed[mode] += 1;
-        totals.wins[mode] += 1;
-        totals.losses[mode] += activity.values.standing.basic.value;
-        totals.wins[mode] -= activity.values.standing.basic.value;
+        if (mode == 48) {
+          //Rumble
+          if (activity.values.standing.basic.value == 0) {
+            totals.wins[mode] += 1;
+          } else {
+            totals.losses[mode] += 1;
+          }
+        } else {
+          totals.wins[mode] += 1;
+          totals.losses[mode] += activity.values.standing.basic.value;
+          totals.wins[mode] -= activity.values.standing.basic.value;
+        }
       } else {
         totals.gamemodeList.push(mode);
         totals.kills[mode] = activity.values.kills.basic.value;
@@ -81,15 +90,26 @@ export function getTotals(activities) {
         totals.score[mode] = activity.values.score.basic.value;
         totals.timePlayedSeconds[mode] = activity.values.timePlayedSeconds.basic.value;
         totals.gamesPlayed[mode] = 1;
-        totals.losses[mode] = activity.values.standing.basic.value;
-        totals.wins[mode] = 1;
-        totals.wins[mode] -= activity.values.standing.basic.value;
+        if (mode == 48) {
+          //Rumble
+          if (activity.values.standing.basic.value == 0) {
+            totals.wins[mode] = 1;
+            totals.losses[mode] = 0;
+          } else {
+            totals.wins[mode] = 0;
+            totals.losses[mode] = 1;
+          }
+        } else {
+          totals.losses[mode] = activity.values.standing.basic.value;
+          totals.wins[mode] = 1;
+          totals.wins[mode] -= activity.values.standing.basic.value;
+        }
       }
   }
 
   return totals;
 }
 
-export function round(number, decimals=5){
+export function round(number, decimals = 5) {
   return Math.round((number + Number.EPSILON) * Math.pow(10, decimals)) / Math.pow(10, decimals);
 }
